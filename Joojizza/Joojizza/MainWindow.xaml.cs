86ef416a1302c3be2f28690220656e5bb2179671
MoShFa <morteza.shahrabii@gmail.com>
2020-06-29 14:36:22 +0430

    designing UI for login page for user

--- Joojizza/Joojizza/MainWindow.xaml.cs
+++ Joojizza/Joojizza/MainWindow.xaml.cs
@@ -13,10 +13,24 @@ namespace Joojizza
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void user_Click(object sender, RoutedEventArgs e)
         {
             SoundPlayer soundPlayer = new SoundPlayer(@"G:\works\university\AP\Joojizza\Joojizza\Joojizza\media.io_zapsplat_multimedia_button_click_fast_plastic_49161.wav");
             soundPlayer.Play();
+            UserLogin userLogin = new UserLogin();
+            userLogin.Show();
+            this.Close();
         }
+
+        private void admin_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"G:\works\university\AP\Joojizza\Joojizza\Joojizza\media.io_zapsplat_multimedia_button_click_fast_plastic_49161.wav");
+            soundPlayer.Play();
+            AdminLogin adminLogin = new AdminLogin();
+            adminLogin.Show();
+            this.Close();
+        }
+
+
     }  
 }
---
+++ Joojizza/Joojizza/UserLogin.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Joojizza
+{
+    /// <summary>
+    /// Interaction logic for UserLoginxaml.xaml
+    /// </summary>
+    public partial class UserLogin : Window
+    {
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
